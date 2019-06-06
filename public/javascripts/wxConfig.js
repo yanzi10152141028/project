e3e44708cdb031b6058cd7179b94f6769634ccb6
Alisa <2622018614@qq.com>
2019-06-06 15:05:36 +0800

    共用的appid和secret及入口

--- public/javascripts/wxConfig.js
+++ public/javascripts/wxConfig.js
@@ -1,6 +1,6 @@
 // 存储appid
 module.exports = {
-    appid:'wx3f4fe1fe7d8ae42c',
-    secret:'44f5748be76d25f662fd8019c986235a',
+    appid:'',
+    secret:'',
     url:'https://api.weixin.qq.com'
 }
